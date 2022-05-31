2da51ecd26e726333270dab89c1a63cea7eac114
varunseshadri <varunseshadri@gmail.com>
2022-05-31 13:56:21 +0200

    to implement, basic setup

---
+++ tf-quantum/channnel_implementation.py
@@ -0,0 +1,18 @@
+import cirq 
+import numpy as np
+
+def cost_function(input_state: np.ndarray, rho: np.ndarray):
+    """
+        Circuit to calculate the fidelity as per the supplementary information in Beer et al.
+    """
+    pass
+
+def bigswap():
+    """
+        Gate required to calculate the cost function
+    """
+    pass
+
+def 
+
+
